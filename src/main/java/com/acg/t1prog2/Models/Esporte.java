5c54208693315b365918cde65801508e68d55350
marquescauee <cauelopesmarques123@gmail.com>
2022-06-09 18:30:03 -0300

    Commit

--- src/main/java/com/acg/t1prog2/Models/Esporte.java
+++ src/main/java/com/acg/t1prog2/Models/Esporte.java
@@ -11,10 +11,19 @@ public abstract class Esporte implements Modalidade {
 
     private int qtdParticipantes;
     private List<Mensalidade> listaMensalidades; 
+    
+    private int codigoEsporte;
+    
+    private static int geradorCodigoEsporte = 0;
 
     protected Esporte(int qtdJogadores) {
         this.listaMensalidades = new ArrayList<>();
+        this.codigoEsporte = gerarCodigoTurma();
         this.qtdParticipantes = qtdJogadores;
+    }
+    
+    public static int gerarCodigoTurma() {
+        return ++geradorCodigoEsporte;
     }
     
     public void addMensalidade(Mensalidade mensalidade) {
@@ -41,4 +50,26 @@ public abstract class Esporte implements Modalidade {
     public String toString() {
         return "Esporte: ";
     } 
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Esporte other = (Esporte) obj;
+        return this.codigoEsporte == other.codigoEsporte;
+    }
+    
 }
